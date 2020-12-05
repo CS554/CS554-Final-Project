fd11b6771b3d06757d4994d410dc03a4b0b8705c
Josh Garner <jgarner413@gmail.com>
2020-12-05 13:00:23 -0500

    updated landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,7 +5,11 @@ import '../App.css';
 function Landing() {
   return (
     <div>
-      <h2>This is the Landing page</h2>
+      
+      <img src="/imgs/take-a-hike-logo.png"></img>
+      <h2>Welcome to Take A Hike!</h2>
+      <p class="intro">Take A Hike! is our CS-554 final project application to help users find hiking trails near them. Either using our locate me feature
+        or typing in your coordinates, Take A Hike! finds the best hiking trails near you! </p>
     </div>
   );
 }
