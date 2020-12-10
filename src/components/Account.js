0e9312ac070781afc60a8f8f2ada8228c5d0c496
Josh Garner <jgarner413@gmail.com>
2020-12-10 17:30:24 -0500

    added basic info to account page

--- src/components/Account.js
+++ src/components/Account.js
@@ -3,13 +3,28 @@ import SignOutButton from './SignOut';
 import '../App.css';
 import ChangePassword from './ChangePassword';
 
+let temp_user = {
+  username: 'jgarner413',
+  email: 'jgarner413@gmail.com',
+  firstName: 'Josh',
+  lastName: 'Garner'
+
+}
+
 function Account() {
   return (
+    <>
     <div>
       <h2>Account Page</h2>
+      <img src="/imgs/hiking_icon.png"></img>
+    <p>User Name: {temp_user.username}</p><br></br>
+    <p>First Name: {temp_user.firstName}</p><br></br>
+    <p>Last Name: {temp_user.lastName}</p><br></br>
+    <p>email: {temp_user.email}</p><br></br>
       <ChangePassword />
       <SignOutButton />
     </div>
+    </>
   );
 }
 
