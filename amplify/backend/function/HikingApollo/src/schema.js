f1c4cfa822b9d777e9079e5435d1fc4ab54fa66d
amrnator <amrsaleh129@gmail.com>
2020-12-12 21:39:20 -0500

    update schema

--- amplify/backend/function/HikingApollo/src/schema.js
+++ amplify/backend/function/HikingApollo/src/schema.js
@@ -1,39 +1,51 @@
 const { gql } = require('apollo-server-lambda');
 
 const typeDefs = gql`
-  type Query {
-    hello: String
-    listTrails(lat: Float!, long: Float!): [Trail]
-  }
-  type Group {
-    groupID: ID!
-    name: String
-    members: [User]
-  }
-  type Trail {
-    trailID: ID!
-    name: String!
-    summary: String
-    difficulty: String
-    rating: Float!
-    num_of_ratings: Int!
-    length: Float
-    ascent: Int
-    descent: Int
-  }
-  type User {
-    id: ID!
-    name: String
-    lat: Float
-    long: Float
-    favorites: [Trail]
-    groups: [Group]
-  }
-  enum difficulty {
-    Easy
-    Medium
-    Hard
-  }
+	type Query {
+		listTrails(lat: Float!, long: Float!): [Trail]
+		getTrailsById(trailId: [ID]!): [Trail]
+		getUser(userId: ID!): User
+		listUsersInGroup(groupId: ID!): [User]
+	}
+	type Mutation {
+		addFavorite(userId: ID!, newFavorite: ID!): User
+		deleteFavorite(userId: ID!, oldFavorite: ID!): User
+		addUserToGroup(groupId: ID!, userId: ID!): User
+		addComment(trailId: ID!, username: String!, text: String!): [Comment]
+	}
+	type Comment {
+		id: ID!
+		username: String!
+		text: String!
+	}
+	type Group {
+		id: ID!
+		name: String
+		members: [ID]
+	}
+	type Trail {
+		id: ID!
+		name: String!
+		summary: String
+		difficulty: String
+		rating: Float!
+		num_of_ratings: Int!
+		length: Float
+		ascent: Int
+		descent: Int
+		img: String
+	}
+	type User {
+		id: ID!
+		name: String
+		favorites: [ID]
+		groups: [ID]
+	}
+	enum difficulty {
+		Easy
+		Medium
+		Hard
+	}
 `;
 
 // const typeDefs = gql`
@@ -43,17 +55,12 @@ const typeDefs = gql`
 //     lat: Float
 //     long: Float
 //   }
-//   type Mutation {
-//     addFavorite(user_id: ID!, new_favorite: ID!): Trail
-//     deleteFavorite(user_id: ID!, new_favorite: ID!): Trail
-//     addUserToGroup(group_id: ID!, user_id: ID!): [User]
-//   }
-//   type Query {
-//     listTrails: [Trail]
-//     listTrail(trail_id: ID!): Trail
-//     listUsersInGroup(group_id: ID!): [User]
-//     updateUserLocation(user_id: ID!, lat: Float!, long: Float!): User
-//   }
+// type Mutation {
+// 	addFavorite(userId: ID!, newFavorite: ID!): Trail
+// 	deleteFavorite(user_id: ID!, new_favorite: ID!): Trail
+// 	addUserToGroup(group_id: ID!, user_id: ID!): [User]
+//   addComment(trail_id: ID!, username: string): [Comment]
+// }
 // `;
 
 module.exports = { typeDefs };
