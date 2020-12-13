9503245d9c44cf6841ccc8cf48316aa905dd04ec
derobertsw <derobertsw@gmail.com>
2020-12-12 22:21:33 -0500

    rollback changes to fix build

--- src/components/Trail.js
+++ src/components/Trail.js
@@ -1,186 +1,57 @@
 import React, { useState, useEffect } from 'react';
-import {
-	Box,
-	Container,
-	List,
-	ListItem,
-	ListItemText,
-	ListItemAvatar,
-	Chip,
-	Avatar,
-	Divider,
-	Paper,
-	Grid
-} from '@material-ui/core';
-import Rating from '@material-ui/lab/Rating';
-import { makeStyles } from '@material-ui/core/styles';
-import CallMadeIcon from '@material-ui/icons/CallMade';
-import DirectionsRunIcon from '@material-ui/icons/DirectionsRun';
-import WbSunnyIcon from '@material-ui/icons/WbSunny';
-import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
-import TextField from '@material-ui/core/TextField';
-
-//import ApolloClient, { gql } from 'apollo-boost';
-//import { useQuery } from '@apollo/client';
 
 import '../App.css';
 
 function Trail(props) {
-	console.log(props.match.params.id);
-
-	const [trailData, setTrailData] = useState([]);
-	//const [error, setError] = useState(false);
-	const [loading, setLoading] = useState(true);
-
-	useEffect(() => {
-		async function getTrail() {
-			let trail = {
-				id: 7011192,
-				name: 'Boulder Skyline Traverse',
-				type: 'Recommended Route',
-				summary: 'The classic long mountain route in Boulder.',
-				difficulty: 'black',
-				stars: 4.7,
-				starVotes: 93,
-				location: 'Superior, Colorado',
-				url:
-					'https://www.hikingproject.com/trail/7011192/boulder-skyline-traverse',
-				imgSqSmall:
-					'https://cdn2.apstatic.com/photos/hike/7039883_sqsmall_1555092747.jpg',
-				imgSmall:
-					'https://cdn2.apstatic.com/photos/hike/7039883_small_1555092747.jpg',
-				imgSmallMed:
-					'https://cdn2.apstatic.com/photos/hike/7039883_smallMed_1555092747.jpg',
-				imgMedium:
-					'https://cdn2.apstatic.com/photos/hike/7039883_medium_1555092747.jpg',
-				length: 17.3,
-				ascent: 5345,
-				descent: -5420,
-				high: 8433,
-				low: 5425,
-				longitude: -105.2582,
-				latitude: 39.9388,
-				conditionStatus: 'All Clear',
-				conditionDetails: 'Dry',
-				conditionDate: '2020-09-16 14:37:11'
-			};
-			setTrailData(trail);
-			setLoading(false);
-		}
-		getTrail();
-	}, [JSON.stringify(trailData)]);
-
-	const useStyles = makeStyles((theme) => ({
-		root: {
-			width: '100%',
-			maxWidth: 360
-		}
-	}));
-
-	const classes = useStyles();
+	let trail = {
+		id: 7011192,
+		name: 'Boulder Skyline Traverse',
+		type: 'Recommended Route',
+		summary: 'The classic long mountain route in Boulder.',
+		difficulty: 'black',
+		stars: 4.7,
+		starVotes: 93,
+		location: 'Superior, Colorado',
+		url:
+			'https://www.hikingproject.com/trail/7011192/boulder-skyline-traverse',
+		imgSqSmall:
+			'https://cdn2.apstatic.com/photos/hike/7039883_sqsmall_1555092747.jpg',
+		imgSmall:
+			'https://cdn2.apstatic.com/photos/hike/7039883_small_1555092747.jpg',
+		imgSmallMed:
+			'https://cdn2.apstatic.com/photos/hike/7039883_smallMed_1555092747.jpg',
+		imgMedium:
+			'https://cdn2.apstatic.com/photos/hike/7039883_medium_1555092747.jpg',
+		length: 17.3,
+		ascent: 5345,
+		descent: -5420,
+		high: 8433,
+		low: 5425,
+		longitude: -105.2582,
+		latitude: 39.9388,
+		conditionStatus: 'All Clear',
+		conditionDetails: 'Dry',
+		conditionDate: '2020-09-16 14:37:11'
+	};
 
 	return (
-		<div>
-			<br />
-			<h1>
-				{trailData.name}
-				<FavoriteBorderIcon />
-			</h1>
-			<Grid container spacing={3}>
-				<Grid item xs={3}>
-					<List className={classes.root}>
-						<ListItem>
-							<Rating
-								defaultValue={trailData.stars}
-								precision={0.5}
-							/>
-							<br />
-							<ListItemText
-								secondary={`${trailData.starVotes} ratings`}
-							/>
-						</ListItem>
-						<Divider variant="inset" component="li" />
-						<ListItem>
-							<ListItemText
-								primary={trailData.difficulty}
-								secondary="Difficulty"
-							/>
-						</ListItem>
-						<Divider variant="inset" component="li" />
-						<ListItem>
-							<ListItemAvatar>
-								<Avatar>
-									<DirectionsRunIcon />
-								</Avatar>
-							</ListItemAvatar>
-							<ListItemText
-								primary={`${trailData.length} miles`}
-								secondary="Total Distance"
-							/>
-						</ListItem>
-						<Divider variant="inset" component="li" />
-						<ListItem>
-							<ListItemAvatar>
-								<Avatar>
-									<CallMadeIcon />
-								</Avatar>
-							</ListItemAvatar>
-							<ListItemText
-								primary={`${trailData.ascent} ft`}
-								secondary="Distance to Ascent"
-							/>
-						</ListItem>
-						<Divider variant="inset" component="li" />
-						<ListItem>
-							<ListItemAvatar>
-								<Avatar>
-									<WbSunnyIcon />
-								</Avatar>
-							</ListItemAvatar>
-							<ListItemText
-								primary={`${trailData.conditionStatus} and ${trailData.conditionDetails}`}
-								secondary={`Current Conditions as of ${trailData.conditionDate}`}
-							/>
-						</ListItem>
-					</List>
-				</Grid>
-				<Grid item xs={9}>
-					<Box
-						maxWidth="sm"
-						height="500px"
-						style={{
-							backgroundImage: `url(${trailData.imgMedium})`
-						}}
-					>
-						<br />
-						<Container>
-							<h2>{trailData.summary}</h2>
-
-							<Paper>
-								<div>
-									<Chip label="Will" />
-									<h3>This hike is awesome!!</h3>
-								</div>
-							</Paper>
-							<Paper>
-								<form
-									className={classes.root}
-									noValidate
-									autoComplete="off"
-								>
-									<TextField
-										id="standard-basic"
-										label="Add a Comment"
-										multiline
-										rowsMax={4}
-									/>
-								</form>
-							</Paper>
-						</Container>
-					</Box>
-				</Grid>
-			</Grid>
-		</div>
+		<>
+			<br></br>
+			<div>
+				<img src={trail.imgMedium} alt="Trail" />
+				<h1>Name: {trail.name}</h1>{' '}
+				<h2>Description: {trail.summary}</h2>
+				<h2>Difficulty: {trail.difficulty}</h2>
+				<h2>
+					Rating: {trail.stars} Stars. {trail.starVotes} Votes
+				</h2>
+				<h2>
+					Condditions: {trail.conditionStatus},{' '}
+					{trail.conditionDetails}. Last updated:{' '}
+					{trail.conditionDate}
+				</h2>
+			</div>
+		</>
 	);
 }
 
