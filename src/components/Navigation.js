65ae0b3e47ef1742ca93983f95828dda951b3108
charkood <charkood@gmail.com>
2020-12-15 19:36:06 -0500

    NavBar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,44 +4,58 @@ import { AuthContext } from '../firebase/Auth';
 import SignOutButton from './SignOut';
 import '../App.css';
 
+import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
+
 const Navigation = () => {
   const { currentUser } = useContext(AuthContext);
   return <div>{currentUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>;
 };
 
+/*style={{ background: 'transparent'}}*/
 const NavigationAuth = () => {
   return (
     <nav className="navigation">
-      <ul>
-        <li>
+    <AppBar position="relative" style={{ background: '#2E3B55' }}>
+      <Toolbar style={{ color: 'black'}}>
+        <IconButton edge="start"  color="black" aria-label="menu">
+        </IconButton>
+        <div className = "links">
+        <Typography variant="h4">
           <NavLink exact to="/" activeClassName="active">
-            Landing
-          </NavLink>
-        </li>
-        <li>
+                Landing
+              </NavLink>
+        </Typography>
+        </div>
+        <div className = "links">
+        <Typography variant="h4">
           <NavLink exact to="/home" activeClassName="active">
-            Home
+                Home
           </NavLink>
-        </li>
-        <li>
-          <NavLink exact to="/profile/:id" activeClassName="active">
-            Account
-          </NavLink>
-        </li>
-        <li>
-          <NavLink exact to="/profile/:id/groups" activeClassName="active">
-            Group
-          </NavLink>
-        </li>
-        <li>
-          <NavLink exact to="/profile/:id/favorites" activeClassName="active">
-            Favorites
-          </NavLink>
-        </li>
-        <li>
-          <SignOutButton />
-        </li>
-      </ul>
+        </Typography>
+        </div>
+        <div className = "links">
+        <Typography variant="h4">
+        <NavLink exact to="/profile/:id" activeClassName="active">
+                Account
+              </NavLink>
+        </Typography>
+        </div>
+        <div className = "links">
+        <Typography variant="h4">
+        <NavLink exact to="/profile/:id/groups" activeClassName="active">
+                Group
+              </NavLink>
+        </Typography>
+        </div>
+        <div className = "links">
+      <Typography variant="h4">
+      <NavLink exact to="/profile/:id/favorites" activeClassName="active">
+              Favorites
+            </NavLink>
+      </Typography>
+      </div>
+      </Toolbar>
+    </AppBar>
     </nav>
   );
 };
@@ -49,25 +63,35 @@ const NavigationAuth = () => {
 const NavigationNonAuth = () => {
   return (
     <nav className="navigation">
-      <ul>
-        <li>
+    <AppBar position="relative" style={{ background: '#2E3B55' }}>
+      <Toolbar style={{ color: 'black'}}>
+        <IconButton edge="start"  color="black" aria-label="menu">
+        </IconButton>
+        <div className = "links">
+        <Typography variant="h4">
           <NavLink exact to="/" activeClassName="active">
-            Landing
-          </NavLink>
-        </li>
-        <li>
-          <NavLink exact to="/signup" activeClassName="active">
-            Sign-up
-          </NavLink>
-        </li>
-
-        <li>
-          <NavLink exact to="/signin" activeClassName="active">
-            Sign-In
-          </NavLink>
-        </li>
-      </ul>
+                Landing
+              </NavLink>
+        </Typography>
+        </div>
+        <Typography variant="h4">
+        <div className = "links">
+        <NavLink exact to="/signup" activeClassName="active">
+          Sign-up
+        </NavLink>
+          </div>
+        </Typography>
+        <Typography variant="h4">
+        <div className = "links">
+        <NavLink exact to="/signin" activeClassName="active">
+          Sign-In
+        </NavLink>
+          </div>
+        </Typography>
+      </Toolbar>
+    </AppBar>
     </nav>
+
   );
 };
 
