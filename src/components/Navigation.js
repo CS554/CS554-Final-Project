fc57afd0262f3665888b2af02d87299b9bfd3e1e
charkood <charkood@gmail.com>
2020-12-15 20:54:26 -0500

    Added Nav Icons

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,24 +3,30 @@ import { NavLink } from 'react-router-dom';
 import { AuthContext } from '../firebase/Auth';
 import SignOutButton from './SignOut';
 import '../App.css';
+import firebase from 'firebase';
+import Icon from '@material-ui/core/Icon';
+import StarIcon from '@material-ui/icons/Star';
+import BookmarkIcon from '@material-ui/icons/Bookmark';
+import GroupIcon from '@material-ui/icons/Group';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import HomeIcon from '@material-ui/icons/Home';
 
 import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
+
 
 const Navigation = () => {
   const { currentUser } = useContext(AuthContext);
   return <div>{currentUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>;
 };
 
-/*style={{ background: 'transparent'}}*/
 const NavigationAuth = () => {
   return (
     <nav className="navigation">
     <AppBar position="relative" style={{ background: '#2E3B55' }}>
-      <Toolbar style={{ color: 'black'}}>
-        <IconButton edge="start"  color="black" aria-label="menu">
-        </IconButton>
+      <Toolbar>
         <div className = "links">
         <Typography variant="h4">
+        <StarIcon> </StarIcon>
           <NavLink exact to="/" activeClassName="active">
                 Landing
               </NavLink>
@@ -28,6 +34,7 @@ const NavigationAuth = () => {
         </div>
         <div className = "links">
         <Typography variant="h4">
+        <HomeIcon> </HomeIcon>
           <NavLink exact to="/home" activeClassName="active">
                 Home
           </NavLink>
@@ -35,6 +42,7 @@ const NavigationAuth = () => {
         </div>
         <div className = "links">
         <Typography variant="h4">
+        <AccountCircleIcon> </AccountCircleIcon>
         <NavLink exact to="/profile/:id" activeClassName="active">
                 Account
               </NavLink>
@@ -42,6 +50,7 @@ const NavigationAuth = () => {
         </div>
         <div className = "links">
         <Typography variant="h4">
+        <GroupIcon> </GroupIcon>
         <NavLink exact to="/profile/:id/groups" activeClassName="active">
                 Group
               </NavLink>
@@ -49,11 +58,17 @@ const NavigationAuth = () => {
         </div>
         <div className = "links">
       <Typography variant="h4">
+      <BookmarkIcon> </BookmarkIcon>
       <NavLink exact to="/profile/:id/favorites" activeClassName="active">
               Favorites
             </NavLink>
       </Typography>
       </div>
+      <div className = "user">
+      <Typography variant="h6">
+      Hi {firebase.auth().currentUser.displayName}
+    </Typography>
+    </div>
       </Toolbar>
     </AppBar>
     </nav>
