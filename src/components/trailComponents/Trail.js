b865125ff751e206e8349a52eef08c490d83a517
derobertsw <derobertsw@gmail.com>
2020-12-16 14:27:10 -0500

    add loading component

--- src/components/trailComponents/Trail.js
+++ src/components/trailComponents/Trail.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Paper, Box, Grid } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-//import CircularProgress from '@material-ui/core/CircularProgress';
 import '../../App.css';
 import Comments from './Comments';
 import TrailData from './TrailData';
 import TrailHeader from './TrailHeader';
 import { useQuery, gql } from '@apollo/react-hooks';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import Loader from 'react-loader-spinner';
 
 function Trail(props) {
 	const [trailData, setTrailData] = useState([]);
@@ -33,7 +33,7 @@ function Trail(props) {
 		}
 	`;
 
-	const { isloading, error, data, refetch } = useQuery(query, {
+	const { isloading, error, data } = useQuery(query, {
 		variables: {
 			trailID: props.match.params.id
 		}
@@ -48,63 +48,60 @@ function Trail(props) {
 		getTrail();
 	}, [data]);
 
-	const useStyles = makeStyles((theme) => ({
-		root: {
-			width: '100%',
-			maxWidth: 360
-		}
-	}));
+	if (isloading || !trailData.name) {
+		return (
+			<Grid container>
+				<Loader type="Grid" color="#00BFFF" height={80} width={80} />
+			</Grid>
+		);
+	}
 
-	const classes = useStyles();
+	if (error) {
+		return <div>Error</div>;
+	}
 
 	return (
 		<div>
-			{!isloading && (
-				<div>
-					<TrailHeader
-						name={trailData.name}
-						trailId={props.match.params.id}
-					/>
+			<TrailHeader
+				name={trailData.name}
+				trailId={props.match.params.id}
+			/>
 
-					<Grid container spacing={1}>
-						<Grid item xs={3}>
-							<TrailData
-								length={trailData.length}
-								rating={trailData.rating}
-								num_of_ratings={trailData.num_of_ratings}
-								difficulty={trailData.difficulty}
-								ascent={trailData.ascent}
-								conditionStatus={trailData.conditionStatus}
-								conditionDetails={trailData.conditionDetails}
-								conditionDate={trailData.conditionDate}
-							/>
-						</Grid>
-						<Grid item xs={4}>
-							<Box
-								maxWidth="sm"
-								height="500px"
-								style={{
-									backgroundImage: `url(${trailData.img})`
-								}}
-							>
-								<Box m={2} pt={3}>
-									<Paper elevation={3}>
-										<h2>{trailData.summary}</h2>
-									</Paper>
-								</Box>
-							</Box>
-						</Grid>
-						<Grid item xs={5}>
-							{!isloading && (
-								<Comments
-									trailId={props.match.params.id}
-									comments={trailData.comments}
-								/>
-							)}
-						</Grid>
-					</Grid>
-				</div>
-			)}
+			<Grid container spacing={1}>
+				<Grid item xs={3}>
+					<TrailData
+						length={trailData.length}
+						rating={trailData.rating}
+						num_of_ratings={trailData.num_of_ratings}
+						difficulty={trailData.difficulty}
+						ascent={trailData.ascent}
+						conditionStatus={trailData.conditionStatus}
+						conditionDetails={trailData.conditionDetails}
+						conditionDate={trailData.conditionDate}
+					/>
+				</Grid>
+				<Grid item xs={4}>
+					<Box
+						maxWidth="sm"
+						height="500px"
+						style={{
+							backgroundImage: `url(${trailData.img})`
+						}}
+					>
+						<Box m={2} pt={3}>
+							<Paper elevation={3}>
+								<h2>{trailData.summary}</h2>
+							</Paper>
+						</Box>
+					</Box>
+				</Grid>
+				<Grid item xs={5}>
+					<Comments
+						trailId={props.match.params.id}
+						comments={trailData.comments}
+					/>
+				</Grid>
+			</Grid>
 		</div>
 	);
 }
