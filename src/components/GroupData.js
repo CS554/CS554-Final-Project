582a386fa78218034bc5b40c9c40da35e6dd19dc
derobertsw <derobertsw@gmail.com>
2020-12-18 22:18:07 -0500

    center group button

--- src/components/GroupData.js
+++ src/components/GroupData.js
@@ -5,7 +5,7 @@ import Loader from 'react-loader-spinner';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
-import { Card, Container } from '@material-ui/core';
+import { Card } from '@material-ui/core';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
@@ -124,16 +124,17 @@ function Groups(props) {
 	let history = useHistory();
 
 	useEffect(() => {
-		async function fetchData(){
-		if (groupData?.members.indexOf(userID) < 0) {
-			setAdd(true);
-			setRemove(false);
-		} else {
-			setAdd(false);
-			setRemove(true);
+		async function fetchData() {
+			if (groupData?.members.indexOf(userID) < 0) {
+				setAdd(true);
+				setRemove(false);
+			} else {
+				setAdd(false);
+				setRemove(true);
+			}
 		}
-	}fetchData();
-}, [userID, groupData?.members,data, userData]);
+		fetchData();
+	}, [userID, groupData?.members, data, userData]);
 
 	if (error) {
 		return <h2>Error 404: Page Not Found</h2>;
